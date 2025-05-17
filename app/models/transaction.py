77890e977ae17770f3307448b777cfbc523c56c3
LesyaLesnichok <lesnikvpole@gmail.com>
2025-05-17 18:11:01 +0200

    added comments for models

--- app/models/transaction.py
+++ app/models/transaction.py
@@ -6,8 +6,11 @@ from datetime import datetime, timezone
 import uuid
 
 class Transaction(Base):
+    
+    # Wskazujemy jak będzie nazywać się tabela w bazie danych
     __tablename__ = "transactions"
 
+    # Tworzymy kolumny wewnątrz tabeli
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)   
     name = Column(String, nullable=False)
     date = Column(DateTime, default=datetime.now(timezone.utc))
@@ -15,6 +18,7 @@ class Transaction(Base):
     category_id = Column(UUID(as_uuid=True), ForeignKey("categories.id"), nullable=True)
     user_id = Column(UUID(as_uuid=True), ForeignKey("users.id"), nullable=False)
 
+    # Tworzymy relację pomiędzy tabelami.
     category = relationship("Category", back_populates="transactions")
     user = relationship("User", back_populates="transactions")
     
--- app/models/user.py
+++ app/models/user.py
@@ -5,11 +5,15 @@ from app.db.database import Base
 import uuid
 
 class User(Base):
+
+    # Wskazujemy jak będzie nazywać się tabela w bazie danych
     __tablename__ = "users"
 
+    # Tworzymy kolumny wewnątrz tabeli
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
     plaid_user_id = Column(String, unique=True, nullable=False)
     email = Column(String, nullable=True)
 
+    # Tworzymy relację pomiędzy tabelami.
     transactions = relationship("Transaction", back_populates="user")
     
