eebfacf975d857c8f08739450616785f29a64c96
lepshiBel <eliseyavvakumov@mail.ru>
2025-05-17 21:30:55 +0200

    Added alembic for automatic migrations. Added .gitignore file. Updated database.py file

--- app/db/database.py
+++ app/db/database.py
@@ -1,8 +1,12 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
-DATABASE_URL = "postgresql:/"
+load_dotenv()  # Ładujemy .env
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
